42c87134f585bca6f6cac87c89433fff07670320
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-07-25 23:42:41 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+export const App = () => {
+
+  useEffect(() => {
+
+    fetch('http://localhost:3000/api/threads', {
+
+
+    })
+
+  }, []);
+
+  return <></>
+
+}
+
