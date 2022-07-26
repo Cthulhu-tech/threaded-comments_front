8f0a3f1d2090ba9e909b939fe4a191ed3948c953
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-07-27 00:40:54 +0300

    footer loader add

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -1,7 +1,53 @@
+import { updateThreadStore } from '../../redux/store/threadInfo';
+import { useInView } from 'react-intersection-observer';
+import { ReduxStore } from '../../interface/interface';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
+
 export const Footer = () => {
 
-    return <footer className="footer layout">
+    const dispatch = useDispatch();
+    const [error, setError] = useState({error: ''});
+    const store = useSelector((state: ReduxStore) => state.THREAD);
 
+    const updateData = () => {
+
+        fetch((process.env.REACT_APP_SERVER as string) + "api/threads", {
+            method: 'POST',
+            mode: 'cors',
+            redirect: 'follow',
+            credentials: "include",
+            headers: {
+                'Content-type': 'application/json',
+                'Accept': 'application/json'
+            },
+
+            body: JSON.stringify({start: store.threads.length, end: store.threads.length + 1})
+
+        })
+        .then((response) => {
+
+            if(!response.ok) response.json().then(json => setError(json));
+
+            return response.json();
+
+        })
+        .then((json) => {
+
+            dispatch(updateThreadStore(json) as any);
+
+        });
+
+    }
+
+    const [ref] = useInView({threshold: 0, onChange: updateData});
+
+    useEffect(() => {}, [error]);
+
+    return <footer ref={error.error !== "limit not found" ? ref : null} className="footer layout">
+
+        {error.error === "limit not found" && "конец"}
 
     </footer>
 
