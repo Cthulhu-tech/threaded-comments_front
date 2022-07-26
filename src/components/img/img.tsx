838291f12f4400a0dbac25ab32a272d10cfa2d34
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-07-27 00:40:36 +0300

    image logic update

--- src/components/img/img.tsx
+++ src/components/img/img.tsx
@@ -4,22 +4,34 @@ import './img.scss';
 
 export const Image = (img: {src: string, alt: string}) => {
 
+    const [load, setLoad] = useState(true);
     const [popup, setPopup] = useState(false);
+    const [loadPopup, setLoadPopup] = useState(true);
     const imgContainerRef = useRef<HTMLDivElement>(null);
     const { image } = UseImageLoad(img.src);
 
-    const SwitchPopupImg = () => setPopup(!popup);
+    const handleOnLoad = () => setLoad(false);
+    const handleOnLoadPopup = () => setLoadPopup(false);
+
+    const SwitchPopupImg = () => {
+
+        setPopup(!popup);
+        setLoadPopup(true);
+
+    }
     
-    useEffect(() => {}, []);
+    useEffect(() => {}, [loadPopup, load]);
 
     return <div onClick={SwitchPopupImg} ref={imgContainerRef} className="sub-container_img">
         <div className="image_container_info">
             <p className="text small">{image.width}x{image.height}</p>
             <p className="text small">{img.alt}</p>
         </div>
-        <img src={img.src} alt={img.alt} className="image" />
+        {load && <div className="loading"/>}
+        <img src={img.src} alt={img.alt} className={!load ? "image" : "onLoading"} onLoad={handleOnLoad}/>
         {popup && <div className="popup_img" onClick={SwitchPopupImg}>
-            <img src={img.src} alt={img.alt} className="image_popup"/>
+            {loadPopup && <div style={{width: image.width, height: image.height}} className="loading"/>}
+            <img src={img.src} alt={img.alt} className={!loadPopup ? "image_popup" : "onLoading"} onLoad={handleOnLoadPopup}/>
         </div>}
     </div>
 
