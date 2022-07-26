77e02eba6a270df2772b59943d28078eebfc3e11
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-07-27 00:41:11 +0300

    thread update

--- src/components/threads/thread.tsx
+++ src/components/threads/thread.tsx
@@ -1,18 +1,14 @@
 import { dateFormating } from "../../utils/dateFormating";
-import { useInView } from "react-intersection-observer";
 import { Threads } from "../../interface/interface";
 import { Image } from "../img/img";
-import { useEffect } from "react";
+import { useEffect } from 'react';
 import './thread.scss';
+
 export const Thread = (thread: Threads) => {
 
     useEffect(() => { }, []);
 
-    const { ref, inView, entry } = useInView({threshold: 0});
-
-    console.log(inView, entry)
-
-    return <section ref={ref} className="thread_container">
+    return <section className="thread_container">
         <article className="thread_info">
             <h1 className="thread_theam text  btn">{thread.name}</h1>
             <p className="text creator">{thread.creator}</p>
