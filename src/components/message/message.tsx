cfdcd8d4ff8b82dcf030c87c6592a72e8d222fdf
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-07-29 22:18:43 +0300

    message update

--- src/components/message/message.tsx
+++ src/components/message/message.tsx
@@ -1,13 +1,13 @@
 import { dateFormating } from "../../utils/dateFormating";
 import { LastMessage } from "../../interface/interface";
+import { useMousePosition } from "../../hook/useMouse";
 import { UseSearch } from "../../hook/useSearch";
 import { Image } from "../img/img";
-import { useState } from 'react';
 import "./message.scss";
 
 export const Message = (data: {msg: LastMessage, className: string}) => {
 
-    const [msgPosition, msgState] = useState<boolean>();
+    const { mousePosition, GetMousePosition } = useMousePosition();
     const { message, searchMessage, deletMessage} = UseSearch();
 
     return <div className={data.className}>
@@ -19,11 +19,10 @@ export const Message = (data: {msg: LastMessage, className: string}) => {
         </div>
         <div className="message_txt">
             <div className="container_message">
-                {data.msg.prev_message && data.msg.prev_message.map((prev) => <p key={prev} className="comments" onMouseOver={()=> {
+                {data.msg.prev_message && data.msg.prev_message.map((prev) => <p key={prev} className="comments" onMouseOver={(event)=> {
                     searchMessage(+prev);
-                    msgState(false);
+                    GetMousePosition(event);
                 }}> » {prev}</p>)}
-                {message && !msgPosition && <div onMouseLeave={deletMessage} className="dynamic_message"><Message {...{msg: message, className: "message sub_message"}}/></div>}
             </div>
             <div className="image_container-msg">
                 {data.msg?.img && data.msg?.img.map((img, i) => <Image key={i} {...{src: img, alt: data.msg.img_name && data.msg?.img_name[i]}} />)}
@@ -31,12 +30,15 @@ export const Message = (data: {msg: LastMessage, className: string}) => {
             <p>{data.msg.text_message}</p>
         </div>
         <div className="container_message">
-            {data.msg.next_message && data.msg.next_message.map((next) => <p key={next} className="comments" onMouseOver={()=> {
+            {data.msg.next_message && data.msg.next_message.map((next) => <p key={next} className="comments" onMouseOver={(event)=> {
                 searchMessage(+next);
-                msgState(true);
+                GetMousePosition(event);
             }} > » {next}</p>)}
-            {message && msgPosition && <div onMouseLeave={deletMessage} className="dynamic_message"><Message {...{msg: message, className: "message sub_message"}}/></div>}
         </div>
+        {message && 
+        <div onMouseLeave={deletMessage} style={{translate: `translate(${mousePosition.x}px, ${mousePosition.y}px)`}} className="dynamic_message">
+            <Message {...{msg: message, className: "message sub_message"}}/>
+        </div>}
     </div>
 
 }
