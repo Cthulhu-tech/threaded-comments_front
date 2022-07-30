703aef9c8125f0ef00fe53b9176029f6b7a03603
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-07-30 20:13:56 +0300

    new logic hook

--- src/hook/useSearch.tsx
+++ src/hook/useSearch.tsx
@@ -1,13 +1,18 @@
-import { LastMessage, ReduxStore } from "../interface/interface";
+import { LastMessage, ReduxStore } from '../interface/interface';
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { useDispatch } from 'react-redux';
+import { updateMessageStore } from '../redux/store/searchMsg';
 
 export const UseSearch = () => {
+
+    const dispatch = useDispatch();
+    const msg = useSelector((store: ReduxStore) => store.MESSAGE);
 
     const [message, setMessage] = useState<LastMessage | null>();
     const store = useSelector((store: ReduxStore) => store.THREAD.threads);
 
-    useEffect(() => {},[])
+    useEffect(() => {console.log(msg)},[])
 
     const searchMessage = (id: number) => {
         
@@ -15,7 +20,17 @@ export const UseSearch = () => {
         .filter((thread) => thread.message.some((msg) => msg.id === id))
         .map(thread => thread.message.filter(msg => msg.id === id));
 
-        setMessage(filteredArray[0][0]);
+        if(msg.message.find(msg=> msg.id === id)){
+
+            console.log('find')
+
+        }else{
+
+            dispatch(updateMessageStore(filteredArray[0]));
+            
+            setMessage(filteredArray[0][0]);
+
+        }
 
     }
 
