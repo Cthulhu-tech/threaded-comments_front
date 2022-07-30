6bfd2eb82f028116795f988e016e9065d1d98201
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-07-30 20:12:39 +0300

    redux stroe update

--- src/redux/root.tsx
+++ src/redux/root.tsx
@@ -1,11 +1,13 @@
 import { combineReducers, createStore } from "redux";
 import { ThreadsStore } from "./store/threadInfo";
+import { MessageStore } from "./store/searchMsg";
 
 
 
 const root = combineReducers({
 
     THREAD: ThreadsStore,
+    MESSAGE: MessageStore
     
 });
 
