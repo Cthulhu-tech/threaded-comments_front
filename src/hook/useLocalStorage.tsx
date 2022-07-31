a5cb450365da9d0f9d1ef97da02ffda22881b228
l lThrackerzod <mentsoff.daniar@yandex.ru>
2022-07-31 23:29:49 +0400

    new logic

--- src/hook/useLocalStorage.tsx
+++ src/hook/useLocalStorage.tsx
@@ -1,26 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const useLocalStorage = (key: string, initialValue: Array<string>) => {
 
-    const [storedValue, setStoredValue] = useState(() => {
-
-        if (typeof window === "undefined") return initialValue;
+    let [storedValue, setStoredValue] = useState<string[]>(() => {
 
         const item = window.localStorage.getItem(key);
-        return item ? JSON.parse(item) : initialValue;
+        return item ? (JSON.parse(item) as string[]) : initialValue;
 
     });
 
     const setValue = (value:string) => {
 
-        const data = [...Array.from(new Set([...storedValue, value]))];
+        let filter = storedValue.find(id => value === id);
 
-        setStoredValue(data);
+        if(filter){
+
+            storedValue = storedValue.filter(id => id !== value);
+
+        }else{
+
+            storedValue = [...storedValue, value];
+
+        }
+
+        setStoredValue(storedValue);
 
         if (typeof window !== "undefined") 
-            window.localStorage.setItem(key, JSON.stringify(data));
+            window.localStorage.setItem(key, JSON.stringify(storedValue));
 
     };
+
+    useEffect(() => {}, [storedValue]);
 
     return [storedValue, setValue];
 
