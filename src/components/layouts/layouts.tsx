04a499a2591b494ae1d20a9f1ccd79803a402bc6
l lThrackerzod <mentsoff.daniar@yandex.ru>
2022-08-01 05:06:50 +0400

    sanding message add

--- src/components/layouts/layouts.tsx
+++ src/components/layouts/layouts.tsx
@@ -1,13 +1,22 @@
+import { SendingMessage } from "../sending/sendingMessage";
+import { ReduxStore } from "../../interface/interface";
 import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 import { Footer } from "./footer";
 import { Header } from "./header";
 import './layout.scss';
 
 export const Layouts = () => {
 
+    const open = useSelector((store: ReduxStore) => store.SENDER.open);
+
+    useEffect(() => {}, [open]);
+
     return <>
         <Header/>
         <Outlet/>
+        {open && <SendingMessage/>}
         <Footer/>
     </>
 
