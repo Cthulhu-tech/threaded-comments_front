70ae09855c6d8f35e9f5f1acc9416d10db07604f
l lThrackerzod <mentsoff.daniar@yandex.ru>
2022-08-02 19:47:10 +0400

    update logic

--- src/redux/store/senderMessage.tsx
+++ src/redux/store/senderMessage.tsx
@@ -1,7 +1,7 @@
-import { Action, SenderMessage,  } from "../../interface/interface";
+import { Action, MessageSenderFrom, SenderFrom  } from "../../interface/interface";
 
 
-const defaultState = {
+const defaultState: MessageSenderFrom = {
 
     from: [],
     message: "",
@@ -14,19 +14,24 @@ const SendMessageHandler = "SEND_MESSAGE_HANDLER";
 const SendAddUserHandler = "SEND_ADD_USER_HANDLER";
 const SendDeleteUserHandler = "SEND_DELETE_USER_HANDLER";
 
-export const SenderMessageStore = (state = defaultState, action:Action<string, string | number | boolean>) => {
+export const SenderMessageStore = (state = defaultState, action:Action<string, string | number | boolean | SenderFrom>) => {
+
     switch (action.type){
         case SendOpenHandler:
             return {...state, open: action.payload as boolean}
         case SendMessageHandler:
             return {...state, message: action.payload as string}
         case SendAddUserHandler:
-            const index = state.from.findIndex((id: number) => id === action.payload as number);
+            const index = (state.from as SenderFrom[]).findIndex((id) => id.msg === (action.payload as SenderFrom).msg);
             if(index === -1)
-            return {...state, from: [...state.from, action.payload as number]}
+            return {...state, from: [...state.from, action.payload as SenderFrom]}
             return state;
         case SendDeleteUserHandler:
-            return {...state, from: [...state.from.filter((userId) => userId !== action.payload as number)]}
+            let filterData = [...state.from.map((userId) => userId.msg !== (action.payload as SenderFrom).msg ? userId : userId.msg === 0 && (action.payload as SenderFrom).msg === 0 ? [] : {msg: 0, thread: userId.thread})];
+            filterData = filterData.filter((element) => Array.isArray(element) !== true);
+            state = {...state, from: filterData.length > 0 ? (filterData as SenderFrom[]) : []}
+            state.from = state.from.filter((value, index, self) => index === self.findIndex((t) => (t.msg === value.msg)));
+            return state;
         default:
             return state;
     }
@@ -35,5 +40,5 @@ export const SenderMessageStore = (state = defaultState, action:Action<string, s
 
 export const openHandlerSender = (payload: boolean) => ({ type: SendOpenHandler, payload });
 export const updateMessageHandlerSender = (payload: string) => ({ type: SendMessageHandler, payload });
-export const addUserHandlerSender = (payload: number) => ({ type: SendAddUserHandler, payload });
-export const deleteUserHandlerSender = (payload: number) => ({ type: SendDeleteUserHandler, payload });
+export const addUserHandlerSender = (payload: SenderFrom) => ({ type: SendAddUserHandler, payload });
+export const deleteUserHandlerSender = (payload: SenderFrom) => ({ type: SendDeleteUserHandler, payload });
