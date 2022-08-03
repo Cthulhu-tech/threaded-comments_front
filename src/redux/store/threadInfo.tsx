161b69bb3563710231dcb9d9246ddcf5bc18d47a
l lThrackerzod <mentsoff.daniar@yandex.ru>
2022-08-03 07:03:26 +0400

    thread logic/delete repeat obj & sort array

--- src/redux/store/threadInfo.tsx
+++ src/redux/store/threadInfo.tsx
@@ -1,6 +1,5 @@
 import { Action, Threads } from "../../interface/interface";
 
-
 const defaultState = {
 
     threads: []
@@ -8,14 +7,19 @@ const defaultState = {
 }
 
 const updateThreads = "THREADS_UPDATE";
-const deleteThreads = "THREADS_DELETE";
 
-export const ThreadsStore = (state = defaultState, action:Action<string, Threads[] | Threads>) => {
+export const ThreadsStore = (state = defaultState, action:Action<string, Threads[]>) => {
     switch (action.type){
-        case updateThreads: 
-            return { threads: [...state.threads, ...(action.payload as Threads[])]}
-        case deleteThreads:
-            return {...(state.threads as Threads[]).filter((thread) => (thread.id as number) !== (action.payload as Threads).id)}
+        case updateThreads:
+            const index = (state.threads as Threads[]).findIndex((thread) => thread.id === (action.payload as Threads[])[0].id);
+
+            if(index === -1)
+                return { threads: [ ...(state.threads as Threads[]), ...(action.payload as Threads[])]}
+
+                return { threads: [ ...(state.threads as Threads[]).filter((thread) => thread.id !== (action.payload as Threads[])[0].id), ...(action.payload as Threads[])]
+                    .sort((a, b) => {
+                        return a.id - b.id;
+                    })}
         default:
             return state;
     }
